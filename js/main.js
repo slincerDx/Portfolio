fb69ab2ff911790e67a30344184ac6736add6291
slincerDx <92074986+slincerDx@users.noreply.github.com>
2023-04-02 23:55:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(function(){
     var mixer = mixitup('.portfolio__content');
     $('.slider__inner').slick({
         dots:true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="/img/arrow-left.svg" alt="arrow-left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/img/arrow-right.svg" alt="arrow-right"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>'
     })
 });
 
